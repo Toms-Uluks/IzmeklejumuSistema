5bb15332c2b71de4f25696567ec20077db480461
Toms-Uluks <ulukst@gmail.com>
2019-05-27 14:43:41 +0300

    finished abdominal cavity

--- src/app/abdominal-cavity-ultrasound/abdominal-cavity-ultrasound.component.ts
+++ src/app/abdominal-cavity-ultrasound/abdominal-cavity-ultrasound.component.ts
@@ -6,7 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./abdominal-cavity-ultrasound.component.sass']
 })
 export class AbdominalCavityUltrasoundComponent implements OnInit {
+  preview: boolean;
 
+  liverSize: string;
+  liverStructure: string;
+  liverEphogenicity: string;
+  liverDucts : string;
+  bladderWalls: string;
+  bladderWidth: string;
+  bladderEcostructure: string;
+  bladderComments: string;
+  pancreaParts: string;
+  pancreaSize: string;
+  pancreaEcostructure: string;
+  pancreaWalls: string;
+  pancreaTail: string;
+  kidneySize: string;
+
+  final: string;
+  doctorName: string;
+
+  name = '';
+  personasKods = '';
+  limfmezgliReason = '';
+  bothchestReason = '';
+  anomalitiesReason = '';
+  finalReason = '';
+  dateTaken;
   constructor() { }
 
   ngOnInit() {
